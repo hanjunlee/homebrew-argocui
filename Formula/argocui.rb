f2b0a3781bb339a1d17755b8d1ffc9191ddfdc74
hanjunlee <opsdownn@gmail.com>
2020-02-09 23:55:46 +0900

    upgrade the version 0.0.2.

--- Formula/argocui.rb
+++ Formula/argocui.rb
@@ -1,15 +1,15 @@
 class Argocui < Formula
   desc "Managing Argo resource by CUI."
   homepage "https://github.com/hanjunlee/argocui"
-  version "0.0.1"
+  version "0.0.2"
   bottle :unneeded
 
   if OS.mac?
-    url "https://github.com/hanjunlee/argocui/releases/download/v0.0.1/argocui_darwin_amd64.tar.gz"
-    sha256 "70680e1b6aabb4149f33ac47fec526ab7e1b809e3bb60ccdc90218f30da3f96e"
+    url "https://github.com/hanjunlee/argocui/releases/download/v0.0.2/argocui_darwin_amd64.tar.gz"
+    sha256 "d118717b3026dd557882cb5d4baa465678e67dc8940fc6dc9652a3011626ad5a"
   elsif OS.linux?
-    url "https://github.com/hanjunlee/argocui/releases/download/v0.0.1/argocui_linux_amd64.tar.gz"
-    sha256 "cbff6d57e81265b9063696b7393b71e78c29e6195228be722358b379ba7c887f"
+    url "https://github.com/hanjunlee/argocui/releases/download/v0.0.2/argocui_linux_amd64.tar.gz"
+    sha256 "960537e93e7700e1d6fbe50907d0fba9b93ebffb4c0dc7b2f99d6df4be59a6de"
   end
 
   def install
