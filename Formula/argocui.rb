25b79c8f8c30bbbbf188c93c1c716f7f2691809b
hanjunlee <opsdownn@gmail.com>
2020-02-10 00:39:20 +0900

    fix sha256.

--- Formula/argocui.rb
+++ Formula/argocui.rb
@@ -6,10 +6,10 @@ class Argocui < Formula
 
   if OS.mac?
     url "https://github.com/hanjunlee/argocui/releases/download/v0.0.3/argocui_darwin_amd64.tar.gz"
-    sha256 "f665a96c6deb6200ed3afe1aaf7f0e2710701cbc5a184619eb109875b4033303"
+    sha256 "c9e95dfc8b4f3811a1dd715b02e89f6596a4b3f8b80c41fef3e0bae56c7c5f19"
   elsif OS.linux?
     url "https://github.com/hanjunlee/argocui/releases/download/v0.0.3/argocui_linux_amd64.tar.gz"
-    sha256 "306ebac236a99d446973fdbb16755082bc3e8a482c0ee6af6ac8c4d40258ffc7"
+    sha256 "e176b9bd70c224179b283e075ba0a1b0a82ad15d9dd1976228882e87f7545a54"
   end
 
   def install
